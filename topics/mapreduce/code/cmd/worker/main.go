bc2026d2912b14775ea33b7d47bf7c1629c2642c
Sagnik <palsagnik2102@gmail.com>
2025-07-09 11:19:20 +0530

    variable number of workers
    Signed-off-by: Sagnik <palsagnik2102@gmail.com>

--- topics/mapreduce/code/cmd/worker/main.go
+++ topics/mapreduce/code/cmd/worker/main.go
@@ -4,13 +4,25 @@ import (
 	"log"
 	"mapreduce/internal/job/invertedindex"
 	"mapreduce/internal/mapreduce"
+	"sync"
 )
 
 func main() {
+
+	const numWorkers = 1
+
 	worker, err := mapreduce.NewWorker("localhost:1234", invertedindex.ProcessingTask)
 	if err != nil {
 		log.Fatal(err)
 	}
 	
-	worker.Run()
+	var wg sync.WaitGroup
+	for i := 0; i < numWorkers; i++ {
+		wg.Add(1)
+		go func ()  {
+			defer wg.Done()
+			worker.Run()
+		}()
+	}
+	wg.Wait()
 } 
